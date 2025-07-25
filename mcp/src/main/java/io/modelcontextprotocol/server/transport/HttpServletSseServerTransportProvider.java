09c8a46f17157dafd65c2250d321b68e0a44dcfb
nolaurence <nolaurence@outlook.com>
2025-07-25 09:19:47 +0800

    feat: 修改doPost和doGet判断路径的方式

--- mcp/src/main/java/io/modelcontextprotocol/server/transport/HttpServletSseServerTransportProvider.java
+++ mcp/src/main/java/io/modelcontextprotocol/server/transport/HttpServletSseServerTransportProvider.java
@@ -170,8 +170,8 @@ public class HttpServletSseServerTransportProvider extends HttpServlet implement
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		String pathInfo = request.getPathInfo();
-		if (!sseEndpoint.equals(pathInfo)) {
+		String requestURI = request.getRequestURI();
+		if (!requestURI.endsWith(sseEndpoint)) {
 			response.sendError(HttpServletResponse.SC_NOT_FOUND);
 			return;
 		}
@@ -225,8 +225,8 @@ public class HttpServletSseServerTransportProvider extends HttpServlet implement
 			return;
 		}
 
-		String pathInfo = request.getPathInfo();
-		if (!messageEndpoint.equals(pathInfo)) {
+		String requestURI = request.getRequestURI();
+		if (!requestURI.endsWith(messageEndpoint)) {
 			response.sendError(HttpServletResponse.SC_NOT_FOUND);
 			return;
 		}
